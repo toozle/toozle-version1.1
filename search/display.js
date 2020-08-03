ee9a25ceec49ca5f343533cc26b8cd47b60e5da9
Toozle <oktoozle@gmail.com>
2020-08-03 21:10:24 +0900

    Add files via upload

---
+++ search/display.js
@@ -0,0 +1,21 @@
+if (queryObject.engine=="google"){
+  document.write('<script async src="https://cse.google.com/cse.js?cx=000978409834300455865:ek8eb5gyomo"></script><div class="gcse-searchresults-only" style="background-color:black;" ></div>');
+}
+if (queryObject.engine=="youtube"){
+  document.write('<script async src="https://cse.google.com/cse.js?cx=000978409834300455865:ajrpyzzjrt4"></script><div class="gcse-searchresults-only"></div>');
+}
+if (queryObject.engine=="twitter"){
+  document.write('<script async src="https://cse.google.com/cse.js?cx=000978409834300455865:6qxarg_sntg"></script><div class="gcse-searchresults-only"></div>');
+}
+if (queryObject.engine=="nicovideo"){
+  document.write('<script async src="https://cse.google.com/cse.js?cx=000978409834300455865:gp9xkvl-ot0"></script><div class="gcse-searchresults-only"></div>');
+}
+if (queryObject.engine=="pornhub"){
+  document.write('<script async src="https://cse.google.com/cse.js?cx=000978409834300455865:tylf5fycecw"></script><div class="gcse-searchresults-only"></div>');
+}
+if (queryObject.engine=="xvideos"){
+  document.write('<script async src="https://cse.google.com/cse.js?cx=000978409834300455865:7icdaydxw2y"></script><div class="gcse-searchresults-only"></div>');
+}
+if (queryObject.engine=="fc2"){
+  document.write('<script async src="https://cse.google.com/cse.js?cx=000978409834300455865:jlpqdbioxuc"></script><div class="gcse-searchresults-only"></div>');
+}
